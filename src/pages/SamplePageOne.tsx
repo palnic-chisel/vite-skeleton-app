90391df3b42d1e8dae87870dad7d7c31d2e62ad3
Marian Sandur <mariansandur_c@tutanota.com>
2021-12-15 15:02:58 +0100

    push test

--- src/pages/SamplePageOne.tsx
+++ src/pages/SamplePageOne.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SamplePageOne = function () {
   return (
-    <div style={{ color: 'black', fontSize: '20px' }}>This is Sample Page 1</div>
+    <div style={{ color: 'black', fontSize: '20px' }}>Test Change</div>
   );
 };
 
